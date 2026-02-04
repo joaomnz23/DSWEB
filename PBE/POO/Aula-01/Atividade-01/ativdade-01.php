283777571731af2768321b2dad5fb73126d2e4be
João Pedro Muniz Silvestre <joo_muniz@yahoo.com>
2026-02-03 22:34:34 -0300

    Finalizando atividade POO
    Finalizando métodos da atividade01 de POO 03/02/2026

--- PBE/POO/Aula-01/Atividade-01/ativdade-01.php
+++ PBE/POO/Aula-01/Atividade-01/ativdade-01.php
@@ -11,21 +11,21 @@ class Estojo {
     public $peso;
 
     // MÉTODO
-    private function abrir(){
+    public function abrir(){
 
         return "Você abriu um estojo que é ".$this->cor;
 
     }
 
     // MÉTODO
-    private function fechar(){   
+    public function fechar(){   
         
         return "Você acabou de fechar um estojo ".$this->capacidade;
 
     }
 
     // MÉTODO 
-    private function organizar(){
+    public function organizar(){
 
         return "Depois que você organizou esse estojo ele tem peso de ".$this->peso;
     }
@@ -38,9 +38,9 @@ $EstojoMelissa->cor = "preto";
 $EstojoMelissa->capacidade = "grande";
 $EstojoMelissa->peso = "400g";
 // EXECUTANDO MÉTODOS
-echo $EstojoMelissa->abriu();
-echo $EstojoMelissa->fechar();
-echo $EstojoMelissa->organizar();
+echo $EstojoMelissa->abrir()."<br>";
+echo $EstojoMelissa->fechar()."<br>";
+echo $EstojoMelissa->organizar()."<br>";
 
 // CLASSE BORRACHA
 class Borracha {
@@ -54,19 +54,30 @@ class Borracha {
 
     // MÉTODO
     public function apagar(){
-
+        return "Depois de você apagar com sua borracha, ela está ".$this->tamanho;
     }
 
     // MÉTODO
     public function emprestar(){
-
+        return "Você acabou de emprestar sua borracha ".$this->cor;
     }
 
     // MÉTODO
     public function morder(){
-
+        return "Não é recomendado morder borrachas de ".$this->material;
     }
 }
+
+// INÍCIO DO OBJETO BORRACHA
+$BorrachaEscolar = new Borracha();
+// INSTÂNCIANDO OBJETO
+$BorrachaEscolar->tamanho = "pequena";
+$BorrachaEscolar->cor = "vermelha";
+$BorrachaEscolar->material = "látex";
+// EXECUTANDO MÉTODOS
+echo $BorrachaEscolar->apagar()."<br>";
+echo $BorrachaEscolar->emprestar()."<br>";
+echo $BorrachaEscolar->morder()."<br>";
 
 // CLASSE PROJETOR
 class Projetor {
@@ -76,23 +87,34 @@ class Projetor {
     public $marca;
     public $tamanho;
     public $cor;
-    public $compatibilidade;
+    public $resolucao;
 
     // MÉTODO
     public function ligar(){
-
+        return "O projetor ".$this->marca." está ligando...";
     }
 
     // MÉTODO
     public function projetar(){
-
+        return "Você está projetando um vídeo com resolução ".$this->resolucao;
     }
 
     // MÉTODO
     public function focar(){
-        
+        return "Tentativa de configuração de uma lente ".$this->lente;
     }
 }
+
+// INÍCIO DO OBJETO PROJETOR
+$ProjetorSenai = new Projetor();
+// INSTÂNCIANDO OBJETO
+$ProjetorSenai->marca = "Epson";
+$ProjetorSenai->resolucao = "4K Full HD";
+$ProjetorSenai->lente = "grande angular";
+// EXECUTANDO MÉTODOS
+echo $ProjetorSenai->ligar()."<br>";
+echo $ProjetorSenai->projetar()."<br>";
+echo $ProjetorSenai->focar()."<br>";
 
 // CLASSE AR-CONDICIONADO
 class ArCondicionado {
@@ -100,31 +122,42 @@ class ArCondicionado {
     // ATRIBUTOS
     public $marca;
     public $cor;
-    public $tamanho;
+    public $voltagem;
     public $modelo;
     public $controle;
 
     // MÉTODO
     public function gelar(){
-
+        return "Seu ar-condicionado ".$this->modelo." está gelando o local";
     }
 
     // MÉTODO
     public function esquentar(){
-
+        return "Para o ar-condicionado ".$this->marca." aquecer a sala, deve estar conectado em uma fonte ".$this->voltagem;
     }
 
     // MÉTODO
     public function ligar(){
-        
+        return "O ar-condicionado ".$this->marca." está ligando...";
     }
 }
+
+// INÍCIO DO OBJETO AR-CONDICIONADO
+$ArCondicionado_quarto = new ArCondicionado();
+// INSTÂNCIANDO OBJETO
+$ArCondicionado_quarto->marca = "Split";
+$ArCondicionado_quarto->modelo = "portátil";
+$ArCondicionado_quarto->voltagem = "110V";
+// EXECUTANDO MÉTODOS
+echo $ArCondicionado_quarto->gelar()."<br>";
+echo $ArCondicionado_quarto->esquentar()."<br>";
+echo $ArCondicionado_quarto->ligar()."<br>";
 
 // CLASSE LIVRO
 class Livro {
 
     // ATRIBUTOS
-    public $tema;
+    public $titulo;
     public $paginas;
     public $capa;
     public $genero;
@@ -132,17 +165,28 @@ class Livro {
 
     // MÉTODO
     public function ler(){
-
+        return "Você está lendo um livro da editora ".$this->editora;
     }
 
     // MÉTODO
-    public function grifar(){
-
+    public function emprestar(){
+        return "Você está emprestando o livro ".$this->titulo;
     }
 
     // MÉTODO
     public function resumir(){
-        
+        return "Você está resumindo todas as ".$this->paginas." páginas deste livro.";
     }
 }
+
+// INÍCIO DO OBJETO LIVRO
+$LivroJoao = new Livro();
+// INSTÂNCIANDO OBJETO
+$LivroJoao->paginas = "250";
+$LivroJoao->titulo = "O Alquimista ";
+$LivroJoao->editora = "Paralela";
+// EXECUTANDO MÉTODOS
+echo $LivroJoao->ler()."<br>";
+echo $LivroJoao->emprestar()."<br>";
+echo $LivroJoao->resumir()."<br>";
 ?>
